aa242ca6d67f8ce186354729080670835012945a
Samim Aktar <samimaktar0773@gmail.com>
2023-11-16 11:04:15 +0530

    First part of context API

---
+++ 08_mini_context/src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import { useContext } from "react";
+import userContext from "../context/UserContext";
+
+function Profile() {
+  const { user } = useContext(userContext);
+
+  if (!user) return <div>Please login</div>;
+  return <div>Welcome {user.username}</div>;
+}
+
+export default Profile;
